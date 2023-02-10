eb31953c9e7b90217df2ac53b0da39829bb79a6c
Fabio <fabiohcsouza@outlook.com.br>
2023-02-10 19:03:31 -0300

    script ps1

---
+++ install.ps1
@@ -0,0 +1,23 @@
+using namespace System.Management.Automation
+using namespace System.Collections.ObjectModel
+
+function install_packages($filepath) {
+    # Verifica se o winget está instalado. 
+    if (!Get-Command winget -ErrorAction SilentlyContinue) {
+        Write-Error "Winget is not installad. Please install winget before running this script."
+        return
+    }
+    
+    # Abre o arquivo e lê cada linha
+    $packages = Get-Content $filepath
+    # Instala cada pacote usando o winget 
+    $packages | ForEach-Object {
+        winget install -e -p$_
+    }
+}
+
+if ($args.Count -eq 1 -and $args[0] -eq "-I") {
+    install_packages("packages.txt")
+} else {
+    Write-Error "Invalid command. Please use -I to install packages"
+}
